d75d324a925fd24fa67ad84e795aa61cd630a539
Pau Sanchez <bladem@gmail.com>
2025-02-15 19:49:47 +0100

    primer commit

---
+++ src/test/java/com/cursos/curso2/Curso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos.curso2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curso2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
