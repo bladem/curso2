39d9414c2a3143aad5275ec793301d40a2e0f458
Pau Sanchez <bladem@gmail.com>
2025-02-15 20:23:57 +0100

    added model to view

--- src/main/java/com/cursos/curso2/infraestructure/controllers/UserController.java
+++ src/main/java/com/cursos/curso2/infraestructure/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.cursos.curso2.infraestructure.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+        model.addAttribute("title", "Hola mundo");
+        model.addAttribute("name", "Pau");
+        model.addAttribute("lastName", "Sanchez");
         return "details";
     }
 
